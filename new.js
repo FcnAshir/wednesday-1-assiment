9b1c32632958f77d1446c455786f25dd1d661d3b
FcnAshir <160272348+FcnAshir@users.noreply.github.com>
2024-04-01 02:32:56 +0500

    asssiment1 completed

---
+++ new.js
@@ -0,0 +1,22 @@
+import inquirer from "inquirer";
+const email = "geo1223@gmail.com";
+const passward = 1234;
+let input = await inquirer.prompt([{
+        name: "as",
+        type: "input",
+        message: "Enter your email!"
+    }]);
+let inputs = await inquirer.prompt([{
+        name: "ash",
+        type: "input",
+        message: "Enter your passward!"
+    }]);
+if (input.as = email && passward) {
+    console.log("Your email is correct");
+}
+if (inputs.ash = email && passward) {
+    console.log("Your passward is correct \n successfully login!");
+}
+else {
+    console.log("Wrong password Or Email");
+}
